6f22f5b55976832a4e3b1672fcce2e0eed53ffee
Luiz Henrique E. N. Santos <luizhenrique.egydio@gmailcom>
2025-05-27 22:54:45 -0300

    all cards created

--- cartas.js
+++ cartas.js
@@ -1,38 +1,102 @@
-
-const listaCartas = [
-    {
-        id: 'yanomami',
-        imgPath: 'src/img/yanomamis.jpg',
-        title: "Povo Yanomami",
-        actionPoints: 3,
-        description: "Uma tribo originária do Brasil que vive na Amazônia",
-        bonus: "Velocidade no crescimento de árvores"
+const cartasJogo = {
+    comuns: [{
+        id: 'povos-originarios',
+        imgPath: 'src/img/povos-originarios.png',
+        title: "Povos Originários",
+        actionPoints: 2,
+        description: "Aqueles que coexistem com a natureza ao seu redor sem abusar",
+        bonus: "Manutenção e preservação da natureza",
+        rarity: "bg-dark",
+        fontColor: "text-white"
     },
-
     {
-        id: 'quaresmeira',
-        imgPath: 'src/img/quaresmeira.jpg',
-        title: "Quaresmeira",
-        actionPoints: 7,
-        description: "Árvore nativa, flores roxas na Quaresma",
-        bonus: "Uma nova árvore"
+        id: 'plantar-arvore',
+        imgPath: 'src/img/plantar-arvore.jpg',
+        title: "Plantar árvore",
+        actionPoints: 2,
+        description: "Uma árvore pode fazer a diferença!",
+        bonus: "Planta uma árvore",
+        rarity: "bg-dark",
+        fontColor: "text-white"
     },
-
     {
+        id: 'caminhar-pedalar',
+        imgPath: 'src/img/bicicleta.jpg',
+        title: "Caminhar ou Pedalar",
+        actionPoints: 2,
+        description: "Meios sustentáveis de locomoção sem poluição.",
+        bonus: "Reduz a destruição ambiental em 1 ponto.",
+        rarity: "bg-dark",
+        fontColor: "text-white"
+    },
+    {
+        id: 'apagar-luzes',
+        imgPath: 'src/img/luzes.jpg',
+        title: "Apagar as Luzes",
+        actionPoints: 2,
+        description: "Economize energia desligando o que não está em uso.",
+        bonus: "Impede o aumento da destruição neste turno.",
+        rarity: "bg-dark",
+        fontColor: "text-white"
+    },
+    {
+        id: 'evitar-plasticos',
+        imgPath: 'src/img/plasticos.jpg',
+        title: "Evitar Plásticos",
+        actionPoints: 2,
+        description: "Escolha reutilizáveis ao invés de descartáveis.",
+        bonus: "Bloqueia o efeito da última carta do vilão.",
+        rarity: "bg-dark",
+        fontColor: "text-white"
+    },
+    ],
+    raras: [{
         id: 'reciclagem',
         imgPath: 'src/img/simbolo-da-reciclagem.jpg',
         title: "Reciclagem",
-        actionPoints: 2,
+        actionPoints: 5,
         description: "(RE)utilizar, (RE)ciclar e (RE)duzir!",
-        bonus: "Pode repetir o bonus da próxima carta"
+        bonus: "Pode repetir o bonus da próxima carta",
+        rarity: "bg-danger",
+        fontColor: "text-dark"
+    }, {
+        id: 'coleta-seletiva',
+        imgPath: 'src/img/coleta-seletiva.jpg',
+        title: "Coleta Seletiva",
+        actionPoints: 5,
+        description: "Separar resíduos corretamente facilita a reciclagem.",
+        bonus: "A próxima carta comum jogada dobra seu efeito.",
+        rarity: "bg-danger",
+        fontColor: "text-dark"
     },
     {
+        id: 'economia-agua',
+        imgPath: 'src/img/economia-agua.jpg',
+        title: "Economia de Água",
+        actionPoints: 5,
+        description: "Evitar desperdícios preserva esse recurso essencial.",
+        bonus: "A próxima carta comum custa 0 pontos de ação.",
+        rarity: "bg-danger",
+        fontColor: "text-dark"
+    },
+    {
+        id: 'educacao-ambiental',
+        imgPath: 'src/img/educacao-ambiental.jpg',
+        title: "Educação Ambiental",
+        actionPoints: 5,
+        description: "Conscientização para transformar atitudes e hábitos.",
+        bonus: "Permite jogar cartas comuns repetidas iguais.",
+        rarity: "bg-danger",
+        fontColor: "text-dark"
+    },],
+    lendarias: [{
         id: 'cultura-maker',
         imgPath: 'src/img/maker.jpg',
         title: "Cultura Maker",
         actionPoints: 10,
         description: "Aprender criando, testando e botando a mão na massa.",
-        bonus: "Pode criar qualquer carta do jogo. Você receberá na próxima rodada"
+        bonus: "Pode criar qualquer carta do jogo. Você receberá na próxima rodada",
+        rarity: "bg-warning"
     },
     {
         id: 'cultura-digital',
@@ -40,7 +104,8 @@ const listaCartas = [
         title: "Cultura Digital",
         actionPoints: 10,
         description: "Integração da tecnologia digital ao cotidiano social e cultural",
-        bonus: "O poder da comunicação audiovisual impede o vilão de agir por 1 turno"
+        bonus: "O poder da comunicação audiovisual impede o vilão de agir por 1 turno",
+        rarity: "bg-warning"
     },
     {
         id: 'prototipagem',
@@ -48,7 +113,8 @@ const listaCartas = [
         title: "Prototipagem",
         actionPoints: 10,
         description: "Criação e teste de objetos em formatos digitais e físicos 3D.",
-        bonus: "Cria soluções sustentáveis que fortalece a humanidade e a natureza. Todos ganham +3 no dado."
+        bonus: "Cria soluções sustentáveis que fortalece a humanidade e a natureza. Todos ganham +3 no dado.",
+        rarity: "bg-warning"
     },
     {
         id: 'robotica',
@@ -56,8 +122,11 @@ const listaCartas = [
         title: "Robótica",
         actionPoints: 10,
         description: "Tecnologia que automatiza tarefas e soluciona desafios humanos.",
-        bonus: "Constrói robôs ecológicos que limpam áreas poluídas. Todos ganham +1 turno"
-    }
-]
+        bonus: "Constrói robôs ecológicos que limpam áreas poluídas. Todos ganham +1 turno",
+        rarity: "bg-warning"
+    }]
+}
 
-export default listaCartas
+const listaCartas = []
+
+export default cartasJogo
--- core.js
+++ core.js
@@ -1,50 +1,102 @@
-import listaCartas from './cartas.js'
+import cartasJogo from './cartas.js'
 
-const btn = document.getElementById('add-card')
+// const btn = document.getElementById('add-card')
 const deck = document.getElementById('deck')
+let quantidadeCartas = deck.children.length
 
-btn.addEventListener('click', () => {
-    deck.append(criarCard())
-})
+const raridade = { comum: .78, raro: .20, lendaria: .02 }
 
 // window.onbeforeunload = function () {
 //     return true;
 // };
 
-function criarCard() {
-    deck.innerHTML = ''
+window.onload = function () {
+    if (quantidadeCartas === 0) {
+        for (let i = 0; i != 5; i++) {
+            deck.append(criarCard(i))
+        }
+    }
+}
 
-    const randomN = Math.ceil(Math.random() * listaCartas.length - 1)
+function criarCard(id) {
 
+    const sorteio = Math.random()
+
+    let cartas = cartasJogo.comuns;
+    switch (sorteio > 0) {
+        case sorteio <= raridade.lendaria:
+            cartas = cartasJogo.lendarias
+            break
+        case sorteio > raridade.lendaria && sorteio <= raridade.comum:
+            cartas = cartasJogo.comuns
+            break;
+        case sorteio > raridade.comum:
+            cartas = cartasJogo.raras
+            break;
+        default:
+            break;
+    }
+    const randomN = Math.ceil(Math.random() * cartas.length - 1)
     const divCard = document.createElement("div");
-    divCard.id = "card";
-    divCard.className = "bg-dark card-background ms-3 position-relative";
+    divCard.id = "card-" + id;
+    divCard.onclick = (c) => {
+        const element = document.getElementById(c.currentTarget.id)
+
+        if (element.parentElement.id === 'board') {
+            document.getElementById('deck').appendChild(element)
+        } else {
+            const board = document.getElementById('board')
+            const boardImgs = board.getElementsByTagName('img')
+            let compareImg = element.getElementsByTagName('img')[0]
+
+
+            if (!boardImgs[0].src.endsWith('src/img/educacao-ambiental.jpg')) {
+                board.appendChild(element)
+            }
+
+            for (let e = 0; e < boardImgs.length; e++) {
+                if (boardImgs[e].src === compareImg.src) {
+                    alert("Você não pode jogar duas cartas iguais.")
+                    return
+                }
+
+                board.appendChild(element)
+            }
+        }
+
+    }
+    divCard.className = `${cartas[randomN].rarity} card-background ms-3 position-relative`;
+    divCard.style.cursor = 'pointer'
+    divCard.style.minHeight = "470px"
 
     const badge = document.createElement("span");
-    badge.textContent = listaCartas[randomN].actionPoints;
+    badge.textContent = cartas[randomN].actionPoints;
     badge.style.width = "80px";
     badge.style.height = "80px";
     badge.className =
-        "d-flex justify-content-center align-items-center fs-1 text-danger badge position-absolute top-0 start-50 translate-middle bg-warning rounded-circle";
+        " d-flex justify-content-center align-items-center fs-1 text-danger badge position-absolute top-0 start-50 translate-middle bg-warning rounded-circle";
 
     const img = document.createElement("img");
-    img.src = listaCartas[randomN].imgPath
+    img.src = cartas[randomN].imgPath
+    img.id = "img-card-deck"
     img.alt = "";
-    img.height = 270;
-    img.width = 270;
+    img.height = 300;
+    img.width = 300;
     img.className = "object-fit-cover card-img-custom";
 
     const h4 = document.createElement("h4");
-    h4.className = "text-white text-uppercase fw-bolder border-bottom mb-0 p-1";
-    h4.textContent = listaCartas[randomN].title;
+    h4.className = `${cartas[randomN].fontColor} text-uppercase text-center h-4 border-bottom mb-0`;
+    h4.textContent = cartas[randomN].title;
 
     const p1 = document.createElement("p");
-    p1.className = "text-white m-0 p-1";
-    p1.textContent = listaCartas[randomN].description;
+    p1.className = `text-white m-0 blockquote fs-6 fst-italic pt-2`;
+    p1.textContent = cartas[randomN].description;
 
     const p2 = document.createElement("p");
-    p2.className = "d-flex text-success pt-3 fst-italic justify-content-center";
-    p2.textContent = listaCartas[randomN].bonus;
+    p2.className = `d-flex align-items-end fs-6 ${cartas[randomN].fontColor == 'text-white' ? 'text-success' : cartas[randomN].fontColor} m-0 justify-content-center`;
+    p2.textContent = cartas[randomN].bonus;
+    p2.style.height = "9vh"
+
 
     divCard.appendChild(badge);
     divCard.appendChild(img);
@@ -53,4 +105,6 @@ function criarCard() {
     divCard.appendChild(p2);
 
     return divCard;
+
 }
+
